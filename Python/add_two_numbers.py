7aa6f9745386e6833df967590427085920efd397
0penth3wind0w <fc0029@yahoo.com.tw>
2018-08-01 22:23:39 +0800

    add #2

---
+++ Python/add_two_numbers.py
@@ -0,0 +1,38 @@
+'''
+    2018/08/01
+    Add Two Numbers: https://leetcode.com/problems/add-two-numbers/description/
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        v1 = extractValue(l1)
+        v2 = extractValue(l2)
+        result = str(v1 + v2)[::-1]
+
+        return createNode(result)
+
+def extractValue(node):
+    value, expon = 0, 0
+    while node != None:
+        value += node.val * 10 ** expon
+        node = node.next
+        expon += 1
+
+    return value
+
+def createNode(value):
+    node = ListNode(int(value[0]))
+    node.next = createNode(value[1:]) if len(value[1:]) != 0 else None
+    
+    return node
--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 | # | Title | Code |
 | -- | ------ | ---------- |
 | 1 | [Two Sum](https://leetcode.com/problems/two-sum/description/) | [Python](./Python/two_sum.py) |  
+| 2 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/description/) | [Python](./Python/add_two_numbers.py) |  
 | 461 | [Hamming Distance](https://leetcode.com/problems/hamming-distance/description/) | [Python](./Python/hamming_distance.py) |  
 | 769 | [Max Chunks To Make Sorted](https://leetcode.com/problems/max-chunks-to-make-sorted/description/) | [Python](./Python/max_chunks_to_make_sorted.py) |  
