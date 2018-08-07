510bcc2bb66c096c85039b4a3b90bd38403b0baa
0penth3wind0w <fc0029@yahoo.com.tw>
2018-08-07 12:00:29 +0800

    add #61

---
+++ Python/rotate_list.py
@@ -0,0 +1,48 @@
+'''
+    2018/08/07
+    Rotate List: https://leetcode.com/problems/rotate-list/description/
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        ptr = head
+        total = 0
+        while ptr != None:
+            ptr = ptr.next
+            total += 1
+        rtime = k%total if total != 0 else 0
+        
+        return rotate(head, total, rtime)
+
+def rotate(head, total, k):
+    if head == None or head.next == None or k == 0:
+        return head
+    cnt = 0
+    newhead = ListNode(None)
+    ptr = head
+    while ptr != None:
+        cnt += 1
+        if total-cnt == k:
+            newhead = ptr.next
+            ptr.next = None
+            break
+        ptr = ptr.next
+    ptr = newhead
+    while ptr != None:
+        if ptr.next == None:
+            ptr.next = head
+            break
+        ptr = ptr.next
+    
+    return newhead
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ My LeetCode solutions.
 | 1 | [Two Sum](https://leetcode.com/problems/two-sum/description/) | [Python](./Python/two_sum.py) |  
 | 2 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/description/) | [Python](./Python/add_two_numbers.py) |  
 | 3 | [Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/description/) | [Python](./Python/longest_substring_without_repeating_characters.py) |  
+| 61 | [Rotate List](https://leetcode.com/problems/rotate-list/description/) | [Python](./Python/rotate_list.py) |  
 | 153 | [Find Minimum in Rotated Sorted Array](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/) | [Python](./Python/find_minimum_in_rotated_sorted_array.py) |  
 | 169 | [Majority Element](https://leetcode.com/problems/majority-element/description/) | [Python](./Python/majority_element.py) |  
 | 307 | [Range Sum Query - Mutable](https://leetcode.com/problems/range-sum-query-mutable/description/) | [Python](./Python/range_sum_query_mutable.py) |  
